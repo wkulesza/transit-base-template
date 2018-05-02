05f20beddd1674aeaf75f2240c62182ff08eae60
Brendan Nee <brendan@blinktag.com>
2018-05-01 21:56:34 -0400

    Correct theme and package name

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -4,7 +4,7 @@
  *
  * Eventually, some of the functionality here could be replaced by core features.
  *
- * @package NWOTA
+ * @package Transit_Base_Template
  */
 
 if ( ! function_exists( 'transit_base_template_posted_on' ) ) :
