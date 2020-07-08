c7a9b5e7853e5e0d6834ee15a118f127a1bfed3d
Claudette Raynor <sleddd@users.noreply.github.com>
2020-07-08 13:42:23 -0700

    Added screenreader functionality to collapse

--- js/collapse.js
+++ js/collapse.js
@@ -30,5 +30,25 @@ const collapse = ( selector, cmd ) => {
   const targets = Array.from( document.querySelectorAll( selector ) );
   targets.forEach( target => {
     target.classList[fnmap[cmd]]('show');
+    var id       = '#' + target.getAttribute('id');
+    var button   = document.querySelector('[data-target="' + id + '"]');
+    var desc     = document.querySelector( id + ' .panel_description');
+    console.log(desc);
+    var expanded =  null;
+    if ( button ) {
+      expanded = button.getAttribute('aria-expanded');
+      if ( expanded === "false" ) {
+        button.setAttribute("aria-expanded", true );
+        if ( desc ) {
+          desc.setAttribute("tabindex", 0);
+        }
+      } 
+      else {
+        button.setAttribute("aria-expanded", false );
+        if ( desc  ) {
+          desc.removeAttribute("tabindex");
+        }
+      }
+    } 
   });
 }
